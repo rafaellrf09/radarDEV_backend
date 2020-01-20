18e7d894e726d8ffb775fce8467fbae1a32eba05
Rafael Lima <rafaellrf09@gmail.com>
2020-01-20 15:52:27 -0300

    home no back

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,10 @@ const SearchController = require('./controllers/SearchController')
 
 const routes = Router()
 
+routes.get("/", (req, res) => {
+    res.send("Página Home");
+});
+
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 

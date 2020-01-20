426689c25eb26a6b77990369277f682c69243926
Rafael Lima <rafaellrf09@gmail.com>
2020-01-20 16:28:23 -0300

    add route delete dev

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ routes.get("/", (req, res) => {
 
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+routes.delete('/devs/:id', DevController.destroy);
 
 routes.get('/search', SearchController.index)
 
